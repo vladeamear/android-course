67dabdde7127edb0277ef832aaa35fdb075e4edf
vladeamear <looser.s.usami@gmail.com>
2023-03-07 21:27:47 +0300

    miasinv: data fetch from openweather api added

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.weatherapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    val LAT: String = "59.8944"
+    val LON: String = "30.2642"
+//    Ширина и долгота Санкт Петербурга
+    val API_ID: String = "16d5ab1b9cb848d337b649f4952d2896"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+    inner class weatherTask(): AsyncTask<String, Void, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            var response: String?
+            try {
+                response = URL("https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&units=metric&appid=$API_ID")
+                    .readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                response = null
+            }
+            return response.toString()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonObj = JSONObject(result)
+                findViewById<TextView>(R.id.text_view).text = jsonObj.toString()
+            } catch (e: Exception) {
+                findViewById<TextView>(R.id.text_view).text = "Error"
+            }
+        }
     }
 }
