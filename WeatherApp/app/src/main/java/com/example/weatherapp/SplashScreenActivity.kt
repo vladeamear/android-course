121ff53de92a184d27c5cb0891c4386b0f584de7
vladeamear <looser.s.usami@gmail.com>
2023-03-12 14:06:33 +0300

    miasinv: new colors, added splash screen

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+//        iv_note.alpha = 0f
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
